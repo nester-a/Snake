b5c02d7ac153adf5f0f3aa7a9f6c6d8919dab8cd
Aleksey <nester.a.l@yandex.ru>
2021-02-02 22:45:32 +0300

    RAM

--- Program.cs
+++ Program.cs
@@ -10,13 +10,53 @@ namespace Snake
     {
         static void Main(string[] args)
         {
+            int x = 1;
+            Func1(x);
+            Console.WriteLine($"Call Func1. x = {x}");
+
+            x = 1;
+            Func2(x);
+            Console.WriteLine($"Call Func2. x = {x}");
+
+            x = 1;
+            Func3(x);
+            Console.WriteLine($"Call Func3. x = {x}");
+
             Point p1 = new Point(1, 3, '*');
-            p1.Draw();
+            Move(p1, 10, 10);
+            Console.WriteLine($"Call Move. p1.x = {p1.x}, p1.y = {p1.y}");
 
             Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+            p1 = p2;
+            p2.x = 8;
+            p2.y = 8;
+            Console.WriteLine($"p1 = p2. p1.x = {p1.x}, p1.y = {p1.y}; p2.x = {p2.x}, p2.y = {p2.y}");
 
+            p1 = new Point(1, 3, '*');
+            Reset(p1);
+            Console.WriteLine($"Call Move. p1.x = {p1.x}, p1.y = {p1.y}; p2.x = {p2.x}, p2.y = {p2.y}");
             Console.ReadLine();
+        }
+        public static void Func1(int value)
+        {
+
+        }
+        public static void Func2(int value)
+        {
+            value++;
+        }
+        public static void Func3(int x)
+        {
+            x++;
+        }
+        public static void Move(Point p, int dx, int dy)
+        {
+            p.x += dx;
+            p.y += dy;
+        }
+        public static void Reset (Point p)
+        {
+            p = new Point();
         }
     }
 }
