2aa960b80a9f2cec219580dacf3af178b178b668
Aleksey <nester.a.l@yandex.ru>
2021-02-03 21:31:28 +0300

    HorizontalLine

---
+++ VerticalLine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class VerticalLine
+    {
+        List<Point> pList;
+
+        public VerticalLine(int yUp, int yDown, int x, char sym)
+        {
+            pList = new List<Point>();
+            for (int y = yUp; y <= yDown; y++)
+            {
+                pList.Add(new Point(x, y, sym));
+            }
+        }
+
+        public void Draw()
+        {
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
